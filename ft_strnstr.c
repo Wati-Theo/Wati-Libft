b474d30c9d9193ef9d98ba194977c89d5fcc8e05
Theo Schlegel <tschlege@z3r3p1.42lyon.fr>
2021-11-07 13:37:55 +0100

    strnstr en cours

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,10 +6,35 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 19:45:27 by tschlege          #+#    #+#             */
-/*   Updated: 2021/11/06 19:58:46 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/07 13:37:20 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack)
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while ((char *)haystack[i] != ((char *)needle)[0] && i < len)
+		i++;
+	while (((char *)needle)[j] == ((char *)haystack)[i] && i < len)
+	{
+		i++;
+		j++;
+	}
+	if (j == ft_strlen(needle))
+		return (((char *)haystack) + (i - j));
+	return (NULL);
+}
+
+int main(void)
+{
+	printf("%s\n", ft_strnstr("Une montee ta peur", "mon", 10));
+}
