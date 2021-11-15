3e6b1a603cb7866629d7cdbd22ea4f515742825b
Theo Schlegel <tschlege@z1r4p4.42lyon.fr>
2021-11-15 14:03:00 +0100

    Correc memcmp memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 17:43:35 by tschlege          #+#    #+#             */
-/*   Updated: 2021/11/08 15:27:49 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/15 14:01:06 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,10 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (!s)
-		return (NULL);
 	while (i < n)
 	{
-		if (((char *)s)[i] == (unsigned char)c)
-			return ((char *)s + i);
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return ((unsigned char *)s + i);
 		i++;
 	}
 	return (NULL);
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 18:49:20 by tschlege          #+#    #+#             */
-/*   Updated: 2021/11/06 19:34:31 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/15 14:02:21 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	i = 0;
 	res = 0;
-	if (!s1 || !s2 || !n)
-		return (0);
 	while (((char *)s1)[i] == ((char *)s2)[i] && i < (n - 1))
 		i++;
 	if (((char *)s1)[i] != ((char *)s2)[i])
