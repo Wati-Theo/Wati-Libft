eaf26da3579dedb5135aaf4235538416a6a523af
Theo Schlegel <tschlege@z1r4p4.42lyon.fr>
2021-11-15 18:09:24 +0100

    ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 17:09:28 by tschlege          #+#    #+#             */
-/*   Updated: 2021/11/05 19:00:29 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/15 17:58:36 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,19 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
-	size_t	sizeofdst;
+	size_t	dst_len;
 
 	i = 0;
-	sizeofdst = ft_strlen(dst);
-	if (dstsize == 0 || dstsize < sizeofdst || src[0] == 0)
-		return (ft_strlen(src) + sizeofdst);
-	while (src[i] && i < dstsize)
+	dst_len = ft_strlen(dst);
+	if (dstsize <= 0)
+		return (ft_strlen(src));
+	if (dstsize <= dst_len)
+		return (dstsize + ft_strlen(src));
+	while (i < (dstsize - dst_len - 1) && i < dstsize)
 	{
-		dst[sizeofdst + i] = src[i];
+		dst[dst_len + i] = src[i];
 		i++;
 	}
-	dst[i] = '\0';
-	return (ft_strlen(src) + sizeofdst);
+	dst[dst_len + i] = '\0';
+	return (ft_strlen(src) + dst_len);
 }
