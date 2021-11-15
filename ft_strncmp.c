de8b0ee17c5fb46882f8014323bf45adab100590
Theo Schlegel <tschlege@z1r4p4.42lyon.fr>
2021-11-15 14:32:38 +0100

    correc strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 13:28:22 by tschlege          #+#    #+#             */
-/*   Updated: 2021/11/06 17:42:44 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/15 14:31:50 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	res = 0;
 	i = 0;
-	if (!s1 || !s2 || !n)
-		return (0);
-	while ((s1[i] == s2[i]) && i < (n - 1))
+	while ((s1[i] == s2[i]) && i < (n - 1) && s1[i] && s2[i])
 		i++;
 	if (s1[i] != s2[i])
 		res = ((unsigned char)s1[i] - (unsigned char)s2[i]);
