88ce7d6cf14b55c110a48a27f3cf15089d79df8b
Theo Schlegel <tschlege@z1r3p5.42lyon.fr>
2021-11-16 12:22:02 +0100

    Correc substr ok ok ok ok

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 18:20:54 by tschlege          #+#    #+#             */
-/*   Updated: 2021/11/16 11:20:56 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/16 12:06:15 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	i = 0;
 	if (start > s_len)
 		return (ft_calloc(1, sizeof(char)));
-	the_nouvelle = ft_calloc((s_len - start) + 1, sizeof(char));
+	the_nouvelle = ft_calloc((len) + 1, sizeof(char));
 	if (!the_nouvelle)
 		return (NULL);
 	while (s[start + i] && i < len)
@@ -33,11 +33,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	the_nouvelle[i] = '\0';
 	return (the_nouvelle);
 }
-
-/*int	main(void)
-{
-	char	*str = "i just want this part #############";
-	
-	//printf("expected output: %s\n\n", substr(str, 5, 10));
-	printf("output: %s\n\n", ft_substr(str, 5, 10));
-}*/
