9ee8cfb1384035120eba02fa614a7d5b72f9965a
Theo Schlegel <tschlege@z4r8p3.42lyon.fr>
2021-11-17 21:38:20 +0100

    better in better

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 18:20:54 by tschlege          #+#    #+#             */
-/*   Updated: 2021/11/17 16:07:12 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/17 17:19:24 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (start > s_len)
 		return (ft_calloc(1, sizeof(char)));
 	if ((s_len - start) > len)
-		the_nouvelle = ft_calloc((len), sizeof(char));
+		the_nouvelle = ft_calloc((len + 1), sizeof(char));
 	else
+	{
 		the_nouvelle = ft_calloc((s_len - start), sizeof(char));
+		len = s_len - start + 1;
+	}
 	if (!the_nouvelle)
 		return (NULL);
 	while (s[start + i] && i < len)
