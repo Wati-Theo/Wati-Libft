57cdee8698c373c83cfd40311a2e7b26aaa98714
Theo Schlegel <tschlege@z1r4p3.42lyon.fr>
2021-11-18 19:05:28 +0100

    ds

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,71 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 19:45:27 by tschlege          #+#    #+#             */
-/*   Updated: 2021/11/17 22:40:08 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/18 16:25:34 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (haystack[i] != needle[0] && haystack[i] && i < len)
+		i++;
+	while (haystack[i] && haystack[j] & i < len)
+	{
+		while (haystack[i] == needle[j] && i < len && haystack[i])
+		{
+			i++;
+			j++;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int	main(void)
+{
+	printf("%s\n", strnstr("lorem ipsum dolor sit amet", "ipsumm", 30));
+	printf("%s\n", ft_strnstr("lorem ipsipsum dolor sit amet", "ipsumm", 30));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+char	*ft_ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
