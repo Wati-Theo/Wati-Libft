bffab5c37acd5878ab8755fd5021d8686e67146a
Theo Schlegel <tschlege@z2r1p3.42lyon.fr>
2021-11-23 18:49:55 +0100

    ah zebii

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 13:51:00 by wati-theo         #+#    #+#             */
-/*   Updated: 2021/11/23 18:22:48 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/23 18:44:03 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,20 @@ t_list	*ft_lstmap(t_list *lst, void *(f)(void *), void (*del)(void *))
 
 	if (!lst)
 		return (NULL);
-	firstnew = lst;
+	firstnew = ft_lstnew(f(lst->content));
+	if (!firstnew)
+		return (NULL);
+	lst = lst->next;
 	while (lst)
 	{
 		element = ft_lstnew(f(lst->content));
+		if (!element)
+		{
+			ft_lstclear(&firstnew, del);
+			return (NULL);
+		}	
 		ft_lstadd_back(&element, element);
-		lst = lst->new;
+		lst = lst->next;
 	}
 	return (firstnew);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 10:42:44 by tschlege          #+#    #+#             */
-/*   Updated: 2021/11/23 16:28:33 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/23 18:46:23 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,6 @@ void	ft_lstadd_back(t_list **alst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(f)(void *), void (*del)(void *));
 
 #endif
