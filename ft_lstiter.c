1c9ab445784da81bbfcebbfc1717d471502b032c
Theo Schlegel <tschlege@z2r1p3.42lyon.fr>
2021-11-23 18:23:06 +0100

    lstmap mid

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -1,30 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/23 13:51:00 by wati-theo         #+#    #+#             */
-/*   Updated: 2021/11/23 18:22:48 by tschlege         ###   ########lyon.fr   */
+/*   Created: 2021/11/23 16:27:45 by tschlege          #+#    #+#             */
+/*   Updated: 2021/11/23 17:02:24 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(f)(void *), void (*del)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	t_list	*firstnew;
-	t_list	*element;
-
 	if (!lst)
-		return (NULL);
-	firstnew = lst;
+		return ;
 	while (lst)
 	{
-		element = ft_lstnew(f(lst->content));
-		ft_lstadd_back(&element, element);
-		lst = lst->new;
+		f(lst->content);
+		lst = lst->next;
 	}
-	return (firstnew);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 10:42:44 by tschlege          #+#    #+#             */
-/*   Updated: 2021/11/22 19:02:45 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/23 16:28:33 by tschlege         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,7 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **alst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
