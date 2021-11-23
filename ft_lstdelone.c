68d2c19dcd280379cfd6290a4d30dc719f2f12ad
¨Wati-Theo¨ <schlegel.theo@outlook.fr>
2021-11-23 13:31:52 +0100

    ft_lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tschlege <tschlege@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: wati-theo <wati-theo@student.42lyon.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 18:37:22 by tschlege          #+#    #+#             */
-/*   Updated: 2021/11/22 18:56:56 by tschlege         ###   ########lyon.fr   */
+/*   Updated: 2021/11/22 21:06:48 by wati-theo        ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	del(lst);
-	free(lst);
+	if (lst)
+	{	
+		del(lst->content);
+		free(lst);
+	}
 }
